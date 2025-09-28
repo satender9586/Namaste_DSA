1fe7844389ede2c74b5571b10157eec0f7c00d56
satender kumar <sksatenderkumar59@gmail.com>
2025-09-28 16:41:31 +0530

    memomize

--- day-4.js
+++ day-4.js
@@ -26,6 +26,30 @@ function findSubArray(arr) {
 
 // console.log(findSubArray([4, 5, 6, 3, 3, 2, 1, 5]));
 
+function sum(n) {
+  let total = 0;
+  for (let i = 0; i < n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+function memoize(callback) {
+  let cache = {};
+  return function (...args) {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    } else {
+      let result = callback(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+}
+
+const eff = memoize(sum);
+console.log(eff(10));
 
 
 
